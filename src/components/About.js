8e86783526776a866dce6bd4170c6efb38bfe1c8
Jacob <jacobdeansd@gmail.com>
2020-04-23 14:26:37 -0700

    updated resume

--- src/components/About.js
+++ src/components/About.js
@@ -33,7 +33,7 @@ export default class About extends Component {
                      {resumeData.address}
                     </span>
                     <br></br>
-                    <a href='https://jgiamanco.github.io/resume/Docs/JGResume8_26.pdf'>Online Resum&eacute;</a>
+                    <a href='https://jgiamanco.github.io/resume/Docs/Giamanco_2020.pdf'>Online Resum&eacute;</a>
        					   </p>
                   </div>
                </div>
