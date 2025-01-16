3fbf7f61717c574e7fb9e58c3521e168c9af2e4d
Jacob <jacobdeansd@gmail.com>
2025-01-16 15:50:52 -0800

    Updated resume data with Python

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,168 +1,174 @@
 let resumeData = {
-    "imagebaseurl":"https://jgiamanco.github.io/",
-    "resume": "https://jgiamanco.github.io/resume/Docs/senior-software-engineer-Jacob-Giamanco.pdf",
-    "name": "Jacob Giamanco",
-    "role": "Senior Software Engineer",
-    "linkedinId":"jacob-giamanco",
-    "skypeid": "Your skypeid",
-    "roleDescription": " ",
-    "socialLinks":[
-        {
-          "key":"1",
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/jacob-giamanco",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "key":"2",
-          "name":"github",
-          "url":"https://github.com/jgiamanco",
-          "className":"fa fa-github"
-        },
-        {
-          "key":"3",
-          "name":"Twitter",
-          "url":"https://twitter.com/GiamancoJ",
-          "className":"fa fa-twitter"
-        }
+  imagebaseurl: "https://jgiamanco.github.io/",
+  resume:
+    "https://jgiamanco.github.io/resume/Docs/senior-software-engineer-Jacob-Giamanco.pdf",
+  name: "Jacob Giamanco",
+  role: "Senior Software Engineer",
+  linkedinId: "jacob-giamanco",
+  skypeid: "Your skypeid",
+  roleDescription: " ",
+  socialLinks: [
+    {
+      key: "1",
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/jacob-giamanco",
+      className: "fa fa-linkedin",
+    },
+    {
+      key: "2",
+      name: "github",
+      url: "https://github.com/jgiamanco",
+      className: "fa fa-github",
+    },
+    {
+      key: "3",
+      name: "Twitter",
+      url: "https://twitter.com/GiamancoJ",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "Valued expert with 5+ years of experience driving significant improvements in technical product management, frontend engineering, web development, and software design. Adept at prioritizing clear communication and strong cross-team collaboration. Skilled in managing and optimizing content for over 60 products, resulting in significant cost savings and improved efficiency. Experienced in implementing automation and scripts to streamline processes. Proven track record of increasing customer engagement and accessibility through innovative solutions. Respected and influential team leader with a natural ability to exceed aggressive goals.",
+  address: "San Diego, CA",
+  website: "https://jgiamanco.now.sh/",
+  education: [
+    {
+      UniversityName: "Grossmont College",
+      specialization: "Fine Art Photography",
+      MonthOfPassing: "June",
+      YearOfPassing: "2013",
+      Achievements: "Featured Courses: ",
+      courses: [
+        "MATH-150 - Intro Comp Program Apps-Math, ",
+        "PHIL-130 - Logic, ",
+        "CSIS-151D - Intro to Computer Graphics, ",
+        "MCOM-110 - Mass Media and Society, ",
+        "BUS-144 - Advertising, ",
+        "GD-105 - Fundamentals of Digital Media",
       ],
-    "aboutme":"Valued expert with 5+ years of experience driving significant improvements in technical product management, frontend engineering, web development, and software design. Adept at prioritizing clear communication and strong cross-team collaboration. Skilled in managing and optimizing content for over 60 products, resulting in significant cost savings and improved efficiency. Experienced in implementing automation and scripts to streamline processes. Proven track record of increasing customer engagement and accessibility through innovative solutions. Respected and influential team leader with a natural ability to exceed aggressive goals.",
-    "address":"San Diego, CA",
-    "website":"https://jgiamanco.now.sh/",
-    "education":[
-      {
-        "UniversityName":"Grossmont College",
-        "specialization":"Fine Art Photography",
-        "MonthOfPassing":"June",
-        "YearOfPassing":"2013",
-        "Achievements":"Featured Courses: ",
-        "courses":[
-           "MATH-150 - Intro Comp Program Apps-Math, ",
-           "PHIL-130 - Logic, ",
-           "CSIS-151D - Intro to Computer Graphics, ",
-           "MCOM-110 - Mass Media and Society, ",
-           "BUS-144 - Advertising, ",
-           "GD-105 - Fundamentals of Digital Media"
-        ]
-      },
-    ],
-    "work":[
-      {
-        "CompanyName":"Client Work/Freelance",
-        "specialization":"Full Stack Web Developer",
-        "MonthOfLeaving":"March",
-        "YearOfLeaving":"2019",
-        "Achievements":""
-      },
-      {
-        "CompanyName":"Jacob Dean Photography",
-        "specialization":"Photographer/Videographer",
-        "MonthOfLeaving":"March",
-        "YearOfLeaving":"2018",
-        "Achievements":""
-      }
-    ],
-    //refer to https://konpa.github.io/devicon/ for skill names
-    "skillsDescription":"",
-    "skills":[
-      {
-        "key":"1",
-        "skillname":"javascript-plain"
-      },
-      {
-        "key":"2",
-        "skillname":"react-original-wordmark"
-      },
-      {
-        "key":"3",
-        "skillname":"typescript-plain"
-      },
-      {
-        "key":"4",
-        "skillname":"nodejs-plain-wordmark"
-      },
-      {
-        "key":"5",
-        "skillname":"sass-original"
-      },
-      {
-        "key":"6",
-        "skillname":"css3-plain-wordmark"
-      },
-      {
-        "key":"7",
-        "skillname":"html5-plain-wordmark"
-      },
-      {
-        "key":"8",
-        "skillname":"express-original-wordmark"
-      },
-      {
-        "key":"9",
-        "skillname":"jquery-plain-wordmark"
-      },
-      {
-        "key":"10",
-        "skillname":"postgresql-plain-wordmark"
-      },
-      {
-        "key":"11",
-        "skillname":"amazonwebservices-plain-wordmark"
-      },
-      {
-        "key":"12",
-        "skillname":"photoshop-line"
-      },
-      {
-        "key":"13",
-        "skillname":"illustrator-line"
-      },
-      {
-        "key":"14",
-        "skillname":"wordpress-plain-wordmark"
-      }
-    ],
-    "portfolio":[
-      {
-        "key":"1",
-        "name":"NFL Roster App",
-        "description":"SPA",
-        "imgurl":"images/rostersMockup.png",
-        "link":"https://nfl-rosters.now.sh",
-        "git": "https://github.com/jgiamanco/nfl-rosters"
-      },
-      {
-        "key":"2",
-        "name":"CRWN Clothing",
-        "description":"Webapp",  
-        "imgurl":"images/crwnEcommerceMockup.png",
-        "link":"https://crwnclothingco.netlify.app",
-        "git": "https://github.com/jgiamanco/crwn-clothing"
-      },
-      // {
-      //   "key":"4",
-      //   "name":"CryptoClash",
-      //   "description":"Data fetching Web App",
-      //   "imgurl":"images/project4.png",
-      //   "link":"https://cryptoclash.herokuapp.com"
-      // }
-      {
-        "key":"3",
-        "name":"Weather Lookup",
-        "description":"SPA with location or search based API fetch",  
-        "imgurl":"images/weatherMockup.png",
-        "link":"https://weatherlookup.now.sh",
-        "git": "https://github.com/jgiamanco/weather-2.0"
-      },
-      {
-        "key":"4",
-        "name":"Face Detection App",
-        "description":"SPA",
-        "imgurl":"images/facialRecogMockup.png",
-        "link": "https://facialdetection.netlify.app",
-        "git": "https://github.com/jgiamanco/face-detection-app"
-      },
-    ]
-  }
-  
-  export default resumeData
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Client Work/Freelance",
+      specialization: "Full Stack Web Developer",
+      MonthOfLeaving: "March",
+      YearOfLeaving: "2019",
+      Achievements: "",
+    },
+    {
+      CompanyName: "Jacob Dean Photography",
+      specialization: "Photographer/Videographer",
+      MonthOfLeaving: "March",
+      YearOfLeaving: "2018",
+      Achievements: "",
+    },
+  ],
+  //refer to https://konpa.github.io/devicon/ for skill names
+  skillsDescription: "",
+  skills: [
+    {
+      key: "1",
+      skillname: "javascript-plain",
+    },
+    {
+      key: "2",
+      skillname: "react-original-wordmark",
+    },
+    {
+      key: "3",
+      skillname: "typescript-plain",
+    },
+    {
+      key: "4",
+      skillname: "python-plain-wordmark",
+    },
+    {
+      key: "5",
+      skillname: "nodejs-plain-wordmark",
+    },
+    {
+      key: "6",
+      skillname: "sass-original",
+    },
+    {
+      key: "7",
+      skillname: "css3-plain-wordmark",
+    },
+    {
+      key: "8",
+      skillname: "html5-plain-wordmark",
+    },
+    {
+      key: "9",
+      skillname: "express-original-wordmark",
+    },
+    {
+      key: "10",
+      skillname: "jquery-plain-wordmark",
+    },
+    {
+      key: "11",
+      skillname: "postgresql-plain-wordmark",
+    },
+    {
+      key: "12",
+      skillname: "amazonwebservices-plain-wordmark",
+    },
+    {
+      key: "13",
+      skillname: "photoshop-line",
+    },
+    {
+      key: "14",
+      skillname: "illustrator-line",
+    },
+    {
+      key: "15",
+      skillname: "wordpress-plain-wordmark",
+    },
+  ],
+  portfolio: [
+    {
+      key: "1",
+      name: "NFL Roster App",
+      description: "SPA",
+      imgurl: "images/rostersMockup.png",
+      link: "https://nfl-rosters.now.sh",
+      git: "https://github.com/jgiamanco/nfl-rosters",
+    },
+    {
+      key: "2",
+      name: "CRWN Clothing",
+      description: "Webapp",
+      imgurl: "images/crwnEcommerceMockup.png",
+      link: "https://crwnclothingco.netlify.app",
+      git: "https://github.com/jgiamanco/crwn-clothing",
+    },
+    // {
+    //   "key":"4",
+    //   "name":"CryptoClash",
+    //   "description":"Data fetching Web App",
+    //   "imgurl":"images/project4.png",
+    //   "link":"https://cryptoclash.herokuapp.com"
+    // }
+    {
+      key: "3",
+      name: "Weather Lookup",
+      description: "SPA with location or search based API fetch",
+      imgurl: "images/weatherMockup.png",
+      link: "https://weatherlookup.now.sh",
+      git: "https://github.com/jgiamanco/weather-2.0",
+    },
+    {
+      key: "4",
+      name: "Face Detection App",
+      description: "SPA",
+      imgurl: "images/facialRecogMockup.png",
+      link: "https://facialdetection.netlify.app",
+      git: "https://github.com/jgiamanco/face-detection-app",
+    },
+  ],
+};
+
+export default resumeData;
