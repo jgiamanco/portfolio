4f9b780f126945684165d166c0ad39a5607fae2b
Jacob <jacobdeansd@gmail.com>
2025-01-27 13:40:45 -0800

    Carousel recommendation letters

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,6 +1,15 @@
 import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "../carousel.css";
 
 const Testimonials = ({ resumeData }) => {
+  const responsive = {
+    0: { items: 1 },
+    600: { items: 1 },
+    1024: { items: 1 },
+  };
+
   return (
     <section id="testimonials">
       <div className="text-container">
@@ -11,19 +20,27 @@ const Testimonials = ({ resumeData }) => {
             </h1>
           </div>
           <div className="ten columns flex-container">
-            <ul className="slides">
+            <AliceCarousel
+              duration={400}
+              autoPlay
+              startIndex={1}
+              buttonsDisabled
+              fadeOutAnimation
+              mouseTrackingEnabled
+              responsive={responsive}
+              autoPlayInterval={4000}
+              autoPlayDirection="ltr"
+            >
               {resumeData.testimonials &&
-                resumeData.testimonials.map((item) => {
-                  return (
-                    <li key={item.name}>
-                      <blockquote>
-                        <p>{item.description}</p>
-                        <cite>{item.name}</cite>
-                      </blockquote>
-                    </li>
-                  );
-                })}
-            </ul>
+                resumeData.testimonials.map((item) => (
+                  <div key={item.name} className="testimonial-item">
+                    <blockquote>
+                      <p>{item.description}</p>
+                      <cite>{item.name}</cite>
+                    </blockquote>
+                  </div>
+                ))}
+            </AliceCarousel>
           </div>
         </div>
       </div>
