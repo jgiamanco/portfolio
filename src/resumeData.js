40f0aa9c4fcd38291ab35428f866260ccaffadbb
Jacob <jacobdeansd@gmail.com>
2025-02-19 14:50:20 -0800

    Update resume

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,7 +1,7 @@
 let resumeData = {
   imagebaseurl: "https://jgiamanco.github.io/",
   resume:
-    "https://jgiamanco.github.io/resume/Docs/jacob_giamanco_resume_2025.pdf",
+    "https://jgiamanco.github.io/resume/Docs/jacob_giamanco_swe_resume_2025.pdf",
   name: "Jacob Giamanco",
   role: "Software Engineer, Developer, Father, and Nerd",
   linkedinId: "jacob-giamanco",
